82e09aced9dc55d6a048bc1dc1f45c6372f6d5c4
Wendell Alves <wendellmccaulay@hotmail.com>
2021-09-17 00:01:57 -0300

    Testing FXML

--- src/main/java/com/example/javafxtutorial/HelloApplication.java
+++ src/main/java/com/example/javafxtutorial/HelloApplication.java
@@ -2,19 +2,26 @@ package com.example.javafxtutorial;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage) {
+        try {
+            Parent parent = FXMLLoader.load(getClass().getResource("View.fxml"));
+            Scene scene = new Scene(parent);
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
